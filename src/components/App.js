7cf0556b8322da573a703c1fa96e1c4c0c99a06b
sushmita4296 <sushmitaprity@gmail.com>
2022-02-27 17:59:07 +0600

    add initial files

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import ContactList from './ContactList';
+import ContactCard from './ContactCard';
+import AddContact from './AddContact';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      {/* <AddContact/>
+      <ContactList/> */}
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+/// tui upload kori de akn main branch dekar dekre nani
