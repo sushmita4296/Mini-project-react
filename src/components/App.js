1f6263c0f9c45e6d3900e0ec4c95caf46ed0fec6
sushmita4296 <sushmitaprity@40gmail.com>
2022-02-28 17:00:23 +0600

    entered data

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,31 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import ContactList from './ContactList';
-import ContactCard from './ContactCard';
+// import ContactCard from './ContactCard';
 import AddContact from './AddContact';
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Bushum",
+      mail: "Nawar@gmail.com"
+    },
+    {
+      id: "2",
+      name: "Asma",
+      mail: "Asma@gmail.com"
+    }
+  ]
+
   return (
-    <div>
+    <div className='ui container'>
       <Header/>
-      {/* <AddContact/>
-      <ContactList/> */}
+      <AddContact/>
+      <ContactList contacts={contacts} />
     </div>
   );
 }
 
 export default App;
 
-
-
-
-
-/// tui upload kori de akn main branch dekar dekre nani
---
+++ src/components/ContactCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ContactCard(){
+  return(
+    <div className="New-cards">
+
+    </div>
+  );
+}
+export default ContactCard;
